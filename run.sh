20583df73fbc784695edfa8d32b7cb4818b30fba
Hennes Brütt <hennzen@gmail.com>
2024-08-15 10:05:36 +0200

    Update shell script

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 # Exit early in case of errror
 set -e 
 cd /home/hennes/workspace/fritz-dect-440-reporter
+echo "git pull"
 /usr/bin/git pull
-/usr/bin npm ci
+echo "npm ci"
+/usr/bin/npm ci
+echo "Run index.js"
 node --env-file=.env index.js
